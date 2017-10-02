a8045473ccf75aab8b4d38e412f758ba64f971d2
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-10-02 15:23:18 +0200

    Done

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const express = require('express'),
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+console.log('Server running...');
+
 app.get('/', (req, res) => {
     res.render('content');
 });
 
 app.get('/auth/google', (req, res) => {
     if (req.query.password === 'filmweb') {
-        res.render('auth', {
+        res.render('start', {
+            name: "Filmweb'u",
             url: 'http://www.filmweb.pl/'
+        });
+    } else if (req.query.password === 'bbc6') {
+        res.render('start', {
+            name: 'BBC Radio 6 Music',
+            url: 'https://www.bbc.co.uk/6music'
         });
     } else {
         res.render('error', {
@@ -25,5 +33,8 @@ app.listen(3000);
 
 app.use((req, res, next) => {
     res.status(404)
-        .send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+        .render('no_response', {
+            info: 'Wybacz, nie mogliśmy odnaleźć tego, czego żądasz...',
+            url: 'http://localhost:3000/'
+        });
 });
